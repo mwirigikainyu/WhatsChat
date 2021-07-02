2b971dc2d677536e54b6bce165a2801b148182bb
mwirigikainyu <mwirigikainyu@gmail.com>
2021-07-02 20:53:56 +0300

    config destructure

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJ5Lpmkwq_2xjiupL6fG6Fw7gHeJ-tU3k",
+  authDomain: "whatsapp-2-610f1.firebaseapp.com",
+  projectId: "whatsapp-2-610f1",
+  storageBucket: "whatsapp-2-610f1.appspot.com",
+  messagingSenderId: "473648020475",
+  appId: "1:473648020475:web:d7fd805538f9df2743695e",
+};
+
+export default firebaseConfig;
--- firebase.js
+++ firebase.js
@@ -1,13 +1,5 @@
 import firebase from "firebase";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAJ5Lpmkwq_2xjiupL6fG6Fw7gHeJ-tU3k",
-  authDomain: "whatsapp-2-610f1.firebaseapp.com",
-  projectId: "whatsapp-2-610f1",
-  storageBucket: "whatsapp-2-610f1.appspot.com",
-  messagingSenderId: "473648020475",
-  appId: "1:473648020475:web:d7fd805538f9df2743695e",
-};
+import firebaseConfig from "./firebase.config";
 
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
